9c020547905b9039e9a52a3fa42309a8b4ebe9c0
Ondra Chaloupka <chalda@chainkeepers.io>
2022-10-12 12:03:13 +0200

    gts fix run

--- tests/governance-spl-tests.ts
+++ tests/governance-spl-tests.ts
@@ -1,16 +1,17 @@
-import * as anchor from "@project-serum/anchor";
-import { Program } from "@project-serum/anchor";
-import { GovernanceSplTests } from "../target/types/governance_spl_tests";
+import * as anchor from '@project-serum/anchor';
+import {Program} from '@project-serum/anchor';
+import {GovernanceSplTests} from '../target/types/governance_spl_tests';
 
-describe("governance-spl-tests", () => {
+describe('governance-spl-tests', () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
-  const program = anchor.workspace.GovernanceSplTests as Program<GovernanceSplTests>;
+  const program = anchor.workspace
+    .GovernanceSplTests as Program<GovernanceSplTests>;
 
-  it("Is initialized!", async () => {
+  it('Is initialized!', async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    console.log('Your transaction signature', tx);
   });
 });
