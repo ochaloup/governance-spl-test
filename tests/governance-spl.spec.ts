0655a656b02d9280c9d688fb215a8cdcc7174f12
Ondra Chaloupka <chalda@chainkeepers.io>
2022-10-24 14:25:09 +0200

    using local solana js utils referrence

--- tests/governance-spl.spec.ts
+++ tests/governance-spl.spec.ts
@@ -1,7 +1,12 @@
 import * as anchor from '@project-serum/anchor';
 import {Program} from '@project-serum/anchor';
 import {GovernanceSplTests} from '../target/types/governance_spl_tests';
-import {MultisigHelper} from '@marinade.finance/solana-test-utils'
+// TODO: RealmHelper needs to be exported by @marinade.finance/solana-test-utils
+// import {RealmHelper} from '@marinade.finance/solana-test-utils'
+
+import { Keypair } from '@solana/web3.js';
+// import { KeypairSignerHelper } from '@marinade.finance/solana-test-utils';
+import  { KeypairSignerHelper  } from '../node_modules/@marinade.finance/solana-test-utils/signer'
 
 describe('governance-spl-tests', () => {
   // Configure the client to use the local cluster.
@@ -12,7 +17,9 @@ describe('governance-spl-tests', () => {
 
   it('Is initialized!', async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log('Your transaction signature', tx);
+    // const tx = await program.methods.initialize().rpc();
+    // console.log('Your transaction signature', tx);
+    const owner = new KeypairSignerHelper(Keypair.generate());
+    console.log("OK", owner)
   });
 });
